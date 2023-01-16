9911db0bd94b9d698dffdd56115f76e0a3389c58
Thunderscrub <topiasxd@gmail.com>
2023-01-17 01:33:29 +0200

    Cleaned up index, only one api endpoint needed.

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { PilotList, getPilots, checkPilotPersistence } from './pilots';
 import cors from 'cors';
-import path from 'path';
 dotenv.config();
 
 let pilotsList: PilotList = {};
@@ -13,15 +12,9 @@ setInterval(async () => {
 }, 2000);
 
 const app: Express = express();
-const port = process.env.PORT;
+
 app.use(cors());
-app.get('/', (req, res) => {
-  res.sendFile;
-});
+
 app.get('/getData', (req: Request, res: Response) => {
   res.send(pilotsList);
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
